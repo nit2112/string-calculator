5270404fab3ed72f403cdfa5797a970bc05e04ac
bishtnitish <nbisht@connamara.com>
2025-01-26 21:46:44 +0530

    sum of empty string

---
+++ lib/string_calculator.rb
@@ -0,0 +1,9 @@
+class StringCalculator
+  def self.add(input)
+     result = 0
+     if (input == "")
+       result = 0
+     end
+     result
+  end
+end
---
+++ spec/calculator_spec.rb
@@ -0,0 +1,11 @@
+require "string_calculator"
+
+describe StringCalculator do
+
+  context "given an empty string" do
+    it "returns zero" do
+      expect(StringCalculator.add("")).to eq(0)
+    end
+  end
+
+end
